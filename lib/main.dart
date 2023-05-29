983d0a5b059489f8368ea7039345c81a6f3b61b5
Joseph McCallum-Nattrass <jmn@jmnetwork.uk>
2023-05-29 20:37:08 +0100

    Started Theme

--- lib/main.dart
+++ lib/main.dart
@@ -11,27 +11,36 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Persona 4 Golden Strategy Guide',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xffffe52c)),
+        scaffoldBackgroundColor: const Color(0XFFFAA622),
+        buttonTheme: const ButtonThemeData(
+          buttonColor: Color(0xFF7FE6EF),
+        ),
+        textTheme: const TextTheme(
+          bodyMedium:
+              TextStyle(color: Colors.white, fontFamily: 'MinervaModernBold'),
+          bodyLarge:
+              TextStyle(color: Colors.white, fontFamily: 'MinervaModernBold'),
+          bodySmall:
+              TextStyle(color: Colors.white, fontFamily: 'MinervaModernBold'),
+          titleLarge:
+              TextStyle(color: Colors.white, fontFamily: 'MinervaModernBold'),
+          titleMedium:
+              TextStyle(color: Colors.white, fontFamily: 'MinervaModernBold'),
+          titleSmall:
+              TextStyle(color: Colors.white, fontFamily: 'MinervaModernBold'),
+        ),
+        appBarTheme: const AppBarTheme(
+            color: Colors.white,
+            titleTextStyle: TextStyle(
+                color: Colors.white,
+                fontFamily: 'MinervaModernBold',
+                fontSize: 20.0)),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Persona 4 Golden Strategy Guide'),
     );
   }
 }
@@ -86,40 +95,7 @@ class _MyHomePageState extends State<MyHomePage> {
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:p4g_stratergy_guide/main.dart';
+import 'package:p4g_strategy_guide/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
